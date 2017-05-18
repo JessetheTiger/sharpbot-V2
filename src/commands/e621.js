c52d2f2e301751c47b0cbbd32836f6ccd2f5d149
JessetheTiger <richardzplayzmc@gmail.com>
2017-05-18 16:11:07 +0200

    updates for discord.js 11.1

--- src/commands/e621.js
+++ src/commands/e621.js
@@ -7,23 +7,23 @@ exports.run = function (bot, msg, args) {
 
     msg.delete();
 
-    msg.channel.sendMessage(':arrows_counterclockwise: Searching...').then(m => {
-        var options = {
-            url: 'https://e621.net/post/index.json?tags= ' + args.join(' ') + ' order:random',
-            json: true,
-            headers: {
-                'User-Agent': '"add your e621 username here" @ E621'
-            },
-            qs: {
-                limit: 3
-            }
+    msg.channel.send(':arrows_counterclockwise: Searching...').then(m => {
+      var options = {
+          url: 'https://e621.net/post/index.json?tags= ' + args.join(' ') + ' order:random',
+          json: true,
+          headers: {
+              'User-Agent': '"your username" @ E621'
+              },
+              qs: {
+              limit: 1
+              }
         };
         request.get(options, (err, res, body) => {
             //Return results if found
             if (!err && Object.keys(`${body[0]}`).length !== 9 && res.statusCode === 200) { // Object.keys(`${body[0]}`).length appears to be 9 when the body is "[]" I've used this to idicate a empty search
                 //m.edit(Object.keys(`${body[0]}`).length );
                 //m.edit(`${body[0]}`);
-                m.edit(`\`\`\`tags =>${args.join(' ')}\`\`\` ${body[0].file_url}`);   //file_url is failure point.
+                m.edit(`\`\`tags => ${args.join(' ')}\`\` ${body[0].file_url}`);   //file_url is failure point.
             }
             //Return an error message if one is given
             else if (err) {
